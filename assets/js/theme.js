8635e3b9d8b67af70b9a90b807d0fb3a3f9253d6
tomsss2k <73535391+tomsss2k@users.noreply.github.com>
2021-11-09 15:40:31 +0200

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -7,23 +7,25 @@ cookieButton.addEventListener("click", () => {
   localStorage.setItem("cookieBannerDisplayed", "true");
 });
 
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 20);
+
 var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 var rootElement = document.documentElement;
 
 function handleScroll() {
-  // Do something on scroll
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
   if (rootElement.scrollTop / scrollTotal < 0.89){
-    // Show button
     scrollToTopBtn.classList.add("showBtn");
   } else {
-    // Hide button
     scrollToTopBtn.classList.remove("showBtn");
   }
 }
 
 function scrollToTop() {
-  // Scroll to top logic
   rootElement.scrollTo({
     top: 0,
     behavior: "smooth"
